b407ce26d8614f914d06dbbe9e470c83680bb794
dsaha007 <143870743+dsaha007@users.noreply.github.com>
2024-01-04 00:31:32 +0530

    Delete js directory

--- js/accordion.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $('.collapse').on('shown.bs.collapse', function(){
-        $(this).parent().find('.fas.fa-plus').removeClass('fa-plus').addClass('fa-minus');
-    }).on('hidden.bs.collapse', function(){
-        $(this).parent().find('.fas.fa-minus').removeClass('fa-minus').addClass('fa-plus');
-    });
-});
