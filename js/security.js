c838287552849fb8f0e049d63d9a7e69cce71320
dsaha007 <143870743+dsaha007@users.noreply.github.com>
2024-01-04 01:21:58 +0530

    Add files via upload

---
+++ js/security.js
@@ -0,0 +1,37 @@
+
+        // Disable right-click and text selection
+        document.addEventListener('contextmenu', event => event.preventDefault());
+
+        // Disable keyboard shortcuts
+        document.onkeydown = function(e) {
+            if(e.keyCode === 123 || (e.ctrlKey && e.shiftKey && e.keyCode === 73)) {
+                return false;
+            }
+        };
+
+        // Disable saving page
+        document.addEventListener('DOMContentLoaded', function() {
+            document.addEventListener('keydown', function(e) {
+                if(e.keyCode == 83 && (navigator.platform.match('Mac') ? e.metaKey : e.ctrlKey)) {
+                    e.preventDefault();
+                }
+            });
+
+            document.addEventListener('keydown', function(e) {
+                if(e.keyCode == 85 && (navigator.platform.match('Mac') ? e.metaKey : e.ctrlKey)) {
+                    e.preventDefault();
+                }
+            });
+        });
+
+        // Disable developer tools
+        document.addEventListener('keydown', function(e) {
+            if (e.keyCode == 123) {
+                e.preventDefault();
+            }
+        });
+
+        window.addEventListener('blur', function() {
+              window.location.href = 'alert.html';
+        });
+    
